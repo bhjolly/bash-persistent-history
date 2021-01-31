25bdc5a0c41048a13babf04f4a49e86fc10c98d1
Ben Jolly <jollyb@landcareresearch.co.nz>
2021-02-01 12:25:46 +1300

    a few tweaks, setup.sh working

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-SRC = dirname "$0"
-HISTORY = "$HOME/.history"
-BASHPH = "$HOME/.bash_ph"
+SRC=$(dirname "$0")
+HISTORY="$HOME/.history"
+BASHPH="$HOME/.bash_ph"
+BASHRC="$HOME/.bashrc"
 
 echo "Setting up 'persistent history'...."
 echo "Actions:"
@@ -11,19 +12,19 @@ echo "  cp $SRC/.bash_ph $BASHPH"
 echo "  chmod +x $BASHPH"
 echo "  echo \" if [ -f $BASHPH ]; then . $BASHPH; fi \" >> $HOME/.bashrc"
 echo ""
-echo "Proceed (ctrl-c to cancel)?"
-
-read 
+read -p "OK? Press [Enter] to proceed, ctrl-c to cancel:"
 
 if [ -d "$HISTORY" ] 
 then 
-    echo "WARNING: $HISTORY already exists, still OK to proceed *ctrl-c to cancel)?"
+    echo "WARNING: $HISTORY already exists"
+    read -p "Press [Enter] to proceed, ctrl-c to cancel:"
 else
     mkdir "$HISTORY"
 fi
 
-cp $SRC/.bash_ph $BASHPH
-chmod +x $BASHPH
-echo " [ -f $BASHPH ] && . $BASHPH " >> $HOME/.bashrc
+cp "$SRC/.bash_ph" "$BASHPH"
+chmod +x "$BASHPH"
+
+echo "[ -f $BASHPH ] && . $BASHPH" >> "$BASHRC"
 
 echo "Done, please close/re-open (or 'source $BASHPH' ) any active terminal windows to start using persistent history"
